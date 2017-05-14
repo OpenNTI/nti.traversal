c3c93a6e66357e15ed8085d245588c79a6e1f604
Carlos Sanchez <carlos.sanchez@nextthought.com>
2017-05-14 02:47:29 +0000

    remove re/exports
    git-svn-id: https://repos.nextthought.com/svn/nti-svn/nti.traversal/trunk@112707 3dfb626b-079f-4ab1-a9f6-4654e5377508

--- src/nti/traversal/__init__.py
+++ src/nti/traversal/__init__.py
@@ -8,6 +8,3 @@ from __future__ import print_function, absolute_import, division
 __docformat__ = "restructuredtext en"
 
 logger = __import__('logging').getLogger(__name__)
-
-from nti.traversal.traversal import find_interface
-from nti.traversal.traversal import find_nearest_site
