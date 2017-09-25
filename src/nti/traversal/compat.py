84b13228364eb40fb8065cd8e8db732ca6826e19
Carlos Sanchez <carlos.sanchez@nextthought.com>
2017-09-25 22:49:36 +0000

    use six to remove unused constants
    git-svn-id: https://repos.nextthought.com/svn/nti-svn/nti.traversal/trunk@122330 3dfb626b-079f-4ab1-a9f6-4654e5377508

--- src/nti/traversal/compat.py
+++ src/nti/traversal/compat.py
@@ -14,7 +14,8 @@ from __future__ import division
 from __future__ import print_function
 from __future__ import absolute_import
 
-import sys
+import six
+from six.moves import urllib_parse
 
 from repoze.lru import lru_cache
 
@@ -28,40 +29,25 @@ try:
 except ImportError:
     PATH_SEGMENT_SAFE = "/~!$&'()*+,;=:@"
 
-PY2 = sys.version_info[0] == 2
-
-if PY2:
-    text_type = unicode
-    binary_type = str
-else:
-    text_type = str
-    binary_type = bytes
-
-if PY2:
-    from urllib import quote
-    _url_quote = quote
-else:
-    from urllib.parse import quote as _url_quote
-
 logger = __import__('logging').getLogger(__name__)
 
 
 def url_quote(val, safe=''):  # bw compat api
     cls = val.__class__
-    if cls is text_type:
+    if cls is six.text_type:
         val = val.encode('utf-8')
-    elif cls is not binary_type:
+    elif cls is not six.binary_type:
         val = str(val).encode('utf-8')
-    return _url_quote(val, safe=safe)
+    return urllib_parse.quote(val, safe=safe)
 
 
-if PY2:
+if six.PY2:
     def native_(s, encoding='latin-1', errors='strict'):
         """ 
         If ``s`` is an instance of ``text_type``, return
         ``s.encode(encoding, errors)``, otherwise return ``str(s)``
         """
-        if isinstance(s, text_type):
+        if isinstance(s, six.text_type):
             return s.encode(encoding, errors)
         return str(s)
 else:
@@ -70,12 +56,12 @@ else:
         If ``s`` is an instance of ``text_type``, return
         ``s``, otherwise return ``str(s, encoding, errors)``
         """
-        if isinstance(s, text_type):
+        if isinstance(s, six.text_type):
             return s
         return str(s, encoding, errors)
 
 
-if PY2:
+if six.PY2:
     def quote_path_segment(segment, safe=PATH_SEGMENT_SAFE):
         """ 
         Return a quoted representation of a 'path segment'
@@ -83,7 +69,7 @@ if PY2:
         try:
             return _segment_cache[(segment, safe)]
         except KeyError:
-            if segment.__class__ is text_type:
+            if segment.__class__ is six.text_type:
                 result = url_quote(segment.encode('utf-8'), safe)
             else:
                 result = url_quote(str(segment), safe)
@@ -97,7 +83,7 @@ else:
         try:
             return _segment_cache[(segment, safe)]
         except KeyError:
-            if segment.__class__ not in (text_type, binary_type):
+            if segment.__class__ not in (six.text_type, six.binary_type):
                 segment = str(segment)
             result = url_quote(native_(segment, 'utf-8'), safe)
             # we don't need a lock to mutate _segment_cache, as the below
