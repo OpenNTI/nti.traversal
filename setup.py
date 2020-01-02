32ad3c0bf6d92971b0b46398c07191cbd649f259
Jason Madden <jamadden@gmail.com>
2020-01-02 09:17:32 -0600

    Preparing release 0.0.1

--- setup.py
+++ setup.py
@@ -20,7 +20,7 @@ def _read(fname):
 
 setup(
     name='nti.traversal',
-    version='0.0.1.dev0',
+    version='0.0.1',
     author='Jason Madden',
     author_email='jason@nextthought.com',
     description="NTI Traversal",
